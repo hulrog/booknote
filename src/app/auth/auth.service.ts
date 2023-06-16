0c7c11ed83af0769434566bfb92ec9b0d11b09cb
Hulrog <petar99t@gmail.com>
2023-06-16 12:47:31 +0200

    pipe za userData

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { tap } from 'rxjs';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -13,8 +15,7 @@ export interface AuthResponseData {
 }
 
 export interface UserData {
-  name?: string;
-  surname?: string;
+  username?: string;
   email: string;
   password: string;
 }
@@ -24,8 +25,10 @@ export interface UserData {
 export class AuthService {
   private _isUserAuthenticated = false;
   private instanceId: number;
+  private user: User;
   constructor(private http: HttpClient) {
     this.instanceId = Date.now();
+    this.user = new User('', '', '', null);
     console.log(`Constructor activated! Instance ID: ${this.instanceId}`);
   }
 
@@ -43,13 +46,39 @@ export class AuthService {
 
   logIn(user: UserData) {
     this._isUserAuthenticated = true;
-    return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.webAPIKey}`,
-      { email: user.email, password: user.password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.webAPIKey}`,
+        { email: user.email, password: user.password, returnSecureToken: true }
+      )
+      .pipe(
+        tap((userData) => {
+          // token istice za
+          // sadasnje vreme u miliseknduma +
+          // istek (po defaultu u firebasu 3600 sekundi * 1000 u miliseknduma)
+          // + operator konvertuje string u broj jer je "3600" string
+          const expirationTime = new Date(
+            new Date().getTime() + +userData.expires * 1000
+          );
+          this.user = new User(
+            userData.localId,
+            userData.email,
+            userData.idToken,
+            expirationTime
+          );
+        })
+      );
   }
 
   logOut() {
     this._isUserAuthenticated = false;
+  }
+
+  getToken() {
+    return this.user.token;
+  }
+
+  getUserId() {
+    return this.user.id;
   }
 }
