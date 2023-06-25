bb6618abdfad833b49900099c8d34ec39d480f2a
Hulrog <petar99t@gmail.com>
2023-06-25 17:50:54 +0200

    profil statistika

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { BooksService } from '../tab1/books.service';
+import { Book } from '../tab1/book.model';
 
 @Component({
   selector: 'app-tab3',
@@ -8,12 +10,45 @@ import { Router } from '@angular/router';
   styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page implements OnInit {
+  genreBreakdown: { genre: string; count: number }[] = [];
+  totalBooks: number = 0;
   userEmail: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private booksService: BooksService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.userEmail = this.authService.getUserEmail();
+    this.calculateBookStatistics();
+  }
+
+  calculateBookStatistics() {
+    this.booksService.getBooks().subscribe((books: Book[]) => {
+      this.totalBooks = books.length;
+      const genreBreakdown: { genre: string; count: number }[] = [];
+
+      // Prebrojavanje knjiga jedna po jedna
+      for (const book of books) {
+        let genreExists = false;
+
+        for (const genreItem of genreBreakdown) {
+          if (genreItem.genre === book.genre) {
+            genreItem.count++;
+            genreExists = true;
+            break;
+          }
+        }
+
+        if (!genreExists) {
+          genreBreakdown.push({ genre: book.genre, count: 1 });
+        }
+      }
+
+      this.genreBreakdown = genreBreakdown;
+    });
   }
 
   logOut() {
