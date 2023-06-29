40a2cd10ec884f3ee4f457c0c9a0c5f639a413be
Hulrog <petar99t@gmail.com>
2023-06-29 12:57:57 +0200

    uklonjene default vrednosti loginu i dodat toast za fail

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -9,27 +10,46 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  // TODO - izbaciti default vrednosti
-  defaultEmail: string = '3user@gmail.com';
-  defaultPassword: string = '3user123';
+  defaultEmail: string = '';
+  defaultPassword: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {}
 
-  onLogin(loginForm: NgForm) {
-    console.log(loginForm);
+  async onLogin(loginForm: NgForm) {
     if (loginForm.valid) {
-      this.authService.logIn(loginForm.value).subscribe((resData) => {
-        console.log('logovanje uspelo');
-        console.log(resData);
-        this.router.navigateByUrl('tabs/tab1');
-        console.log(this.authService);
-      });
+      this.authService.logIn(loginForm.value).subscribe(
+        (resData) => {
+          console.log('logovanje uspelo');
+          console.log(resData);
+          this.router.navigateByUrl('tabs/tab1');
+          console.log(this.authService);
+        },
+        (error) => {
+          this.presentToast('Failed to log in');
+        }
+      );
+    } else {
+      this.presentToast('Invalid form');
     }
   }
 
   goToRegister() {
     this.router.navigateByUrl('/register');
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      color: 'danger',
+    });
+    toast.present();
   }
 }
